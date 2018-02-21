1a73fcfcfa852fc21017cb5103decf2c09f6f54e
Lol <Loler@gmail.com>
2018-02-21 17:21:33 +0300

    New POOPY factorial function

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
-/* 
- * File:   main.cpp
- * Author: Пользователь
- *
- * Created on 26 Январь 2018 г., 16:59
- */
-
-#include <cstdlib>
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 int n;
 unsigned long long fact(int n){
-    unsigned long long result = 1;
-    for(int i = 2; i <= n; i++)
-        result *= i;
-    return result;
+    if(n == 1) return 1;
+    return fact(n-1)*n;
 }
 int main(int argc, char** argv) {
     cin >> n;
